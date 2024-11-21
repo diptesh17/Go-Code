39eb1ac570714b8192124968ef6688fd8dd1cedd
Diptesh <dipteshdde22@gmail.com>
2024-11-21 08:34:51 +0530

    Variables + capitalization

---
+++ 03/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("String : ")
+
+	// string
+	var name string = "Diptesh"
+	var surname = "Deore"
+	fmt.Println(name + " " + surname)
+
+	fmt.Println("Int : ")
+
+	// int
+	var rollnumber int = 14
+	var prn = 121
+	fmt.Println(rollnumber)
+	fmt.Println(prn)
+
+	fmt.Println("Float : ")
+
+	// float
+	var marks float64 = 14.04
+	fmt.Println(marks)
+
+	// Simple way
+	person := "Diptesh Deore"
+	fmt.Println((person))
+}
