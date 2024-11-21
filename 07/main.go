02f94c47b0f0a4a74cdebc312066395abd89951e
Diptesh <dipteshdde22@gmail.com>
2024-11-21 17:29:57 +0530

    Error handling

---
+++ 07/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+// If want to return string
+// func divide(a, b float64) (float64, string) {
+
+// 	if b == 0 {
+// 		return 0, "Deno is 0"
+// 	}
+
+// 	return a / b, "nil"
+// }
+
+func divide(a, b float64) (float64, error) {
+
+	if b == 0 {
+		return 0, fmt.Errorf("Deno is 0")
+	}
+
+	return a / b, nil
+}
+
+func main() {
+	fmt.Println("Error Handling :) ")
+
+	ans, err := divide(10, 0)
+
+	if err == nil {
+		fmt.Println(ans)
+	}
+
+	fmt.Println(err)
+
+}
